26efa2b6f4243ee51ef40ee50046d450687f1e4d
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-03-10 13:29:29 -0700

    greg suggestions

--- 0.download_data/download_figshare.py
+++ 0.download_data/download_figshare.py
@@ -9,10 +9,10 @@ import zipfile
 def download_figshare(
     figshare_id: str,
     output_file: pathlib.Path,
-    output_dir: pathlib.Path,
     metadata_dir: pathlib.Path,
     figshare_url: str = "https://ndownloader.figshare.com/files/",
     unzip_files: bool = False,
+    output_dir: pathlib.Path = None,
 ):
     """
     Download the provided figshare resource and extract the files from Figshare. Extract the downloaded
@@ -23,21 +23,22 @@ def download_figshare(
     figshare_id : str
         string of numbers that corresponds to the figshare identifier
     output_file : pathlib.Path
-        the location and file name for the downloaded contents from Figshare
-    output_dir : pathlib.Path
-        path to directory for images
+        the location and file/folder name for the downloaded contents from Figshare (in the case of zip files, this will be a temp folder and is removed
+        after extraction)
     metadata_dir : pathlib.Path
         path to directory for metadata
     figshare_url: str, default "https://ndownloader.figshare.com/files/"
         the location of where the figshare id is stored
     unzip_files: bool, default False
         if set to True, then the expected download from Figshare is a zip file which will be unzipped
+    output_dir : pathlib.Path, optional
+        path to directory to extract images and metadata from zip file to
     """
     # access the url and download the zip file from figshare containing files for plate (images + metadata)
     urllib.request.urlretrieve(f"{figshare_url}/{figshare_id}", output_file)
 
-    if unzip_files==True:
-    # find the zip file and then extract it into the specific folder
+    if unzip_files:
+        # find the zip file and then extract it into the specific folder
         with zipfile.ZipFile(output_file, "r") as zip_files:
             zip_files.extractall(output_dir)
 
@@ -52,9 +53,9 @@ def download_figshare(
         )
 
     # glob the metadata files together to then copy and remove
-    metadata_files = glob.glob(f'{output_dir}/*.csv')
+    metadata_files = glob.glob(f"{output_dir}/*.csv")
     for files in metadata_files:
-        # copy the metadata downloaded from Figshare into the metadata dir (must use ../ to go back one to access this directory from the plate folder) 
+        # copy the metadata downloaded from Figshare into the metadata dir (must use ../ to go back one to access this directory from the plate folder)
         # which will overwrite the GitHub versioned metadata
         shutil.copy(files, str(metadata_dir))
         # remove the metadata from the plate directory to make it an images only directory
