0e9837e51ed49dfef739b9e4b345f6073f95e0d6
Jenna Tomkinson <jenna.tomkinson@ucdenver.edu>
2023-04-14 09:38:55 -0600

    add plate 3 analysis without run

--- 2.cellprofiler_analysis/scripts/nf1_analysis.py
+++ 2.cellprofiler_analysis/scripts/nf1_analysis.py
@@ -20,16 +20,20 @@ from utils import cp_utils
 # In[2]:
 
 
-path_to_pipeline = pathlib.Path("NF1_analysis.cppipe").absolute()
-
-path_to_output = pathlib.Path("analysis_output").absolute()
+path_to_output = pathlib.Path("./analysis_output").resolve(strict=True)
 
 plates_info_dictionary = {
     "Plate_1": {
-        "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_1/").absolute(),
+        "path_to_pipeline" : pathlib.Path("NF1_analysis_plate1_plate2.cppipe").resolve(strict=True),
+        "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_1/").resolve(strict=True),
     },
     "Plate_2": {
-        "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_2/").absolute(), 
+        "path_to_pipeline" : pathlib.Path("NF1_analysis_plate1_plate2.cppipe").resolve(strict=True),
+        "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_2/").resolve(strict=True), 
+    },
+    "Plate_3": {
+        "path_to_pipeline" : pathlib.Path("NF1_analysis_plate3_plate3'.cppipe").resolve(strict=True),
+        "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_3/").resolve(strict=True), 
     },
 }
 
@@ -43,6 +47,7 @@ plates_info_dictionary = {
 
 # run through each plate with each set of paths based on dictionary
 for plate, info in plates_info_dictionary.items():
+    path_to_pipeline = info["path_to_pipeline"]
     path_to_images = info["path_to_images"]
     print(f"Running analysis on {plate}!")
 
