ae3403d13af9b9d2c0e96994f1d823d420b34b40
Jenna Tomkinson <jenna.tomkinson@ucdenver.edu>
2023-04-17 09:51:38 -0600

    clarifiy pipeline names

--- 2.cellprofiler_analysis/scripts/nf1_analysis.py
+++ 2.cellprofiler_analysis/scripts/nf1_analysis.py
@@ -24,14 +24,17 @@ path_to_output = pathlib.Path("./analysis_output").resolve(strict=True)
 
 plates_info_dictionary = {
     "Plate_1": {
+        # this pipeline is specific to plates 1 and 2
         "path_to_pipeline" : pathlib.Path("NF1_analysis_plate1_plate2.cppipe").resolve(strict=True),
         "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_1/").resolve(strict=True),
     },
     "Plate_2": {
+        # this pipeline is specific to plates 1 and 2
         "path_to_pipeline" : pathlib.Path("NF1_analysis_plate1_plate2.cppipe").resolve(strict=True),
         "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_2/").resolve(strict=True), 
     },
     "Plate_3": {
+        # this pipeline is specific to plates 3 and 3'
         "path_to_pipeline" : pathlib.Path("NF1_analysis_plate3_plate3'.cppipe").resolve(strict=True),
         "path_to_images": pathlib.Path("../1.cellprofiler_ic/Corrected_Plate_3/").resolve(strict=True), 
     },
