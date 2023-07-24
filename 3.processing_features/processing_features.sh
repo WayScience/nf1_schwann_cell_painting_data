fb3d49483350dbda185c8ffa5158ad0f4a1e4f38
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-07-24 14:11:44 -0600

    rename files for clarity

--- 3.processing_features/processing_features.sh
+++ 3.processing_features/processing_features.sh
@@ -11,6 +11,6 @@ jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
 # run the python scripts in order (from convert+merge, aggregate, annotate, normalize, and feature select)
 python scripts/0.merge_sc_cytotable.py 
 python scripts/1.aggregate_sc.py
-python scripts/2.annotate_sc.py
-python scripts/3.normalize_sc.py
-python scripts/4.feature_select_sc.py
+python scripts/2.annotate.py
+python scripts/3.normalize.py
+python scripts/4.feature_select.py
