8b2c55d1f34ac0d5172054bfcdee0712253ce5b6
gwaybio <gregory.way@gmail.com>
2023-08-04 13:04:24 -0600

    execute ipynb directly and chmod +x

--- 3.processing_features/processing_features.sh
+++ 3.processing_features/processing_features.sh
@@ -2,15 +2,28 @@
 
 # initialize the correct shell for your machine to allow conda to work (see README for note on shell names)
 conda init bash
+
 # activate the main conda environment
 conda activate nf1_cellpainting_data
 
 # convert all notebooks to python files into the scripts folder
 jupyter nbconvert --to python --output-dir=scripts/ *.ipynb
 
-# run the python scripts in order (from convert+merge, aggregate, annotate, normalize, and feature select)
-python scripts/0.merge_sc_cytotable.py 
-python scripts/1.aggregate_sc.py
-python scripts/2.annotate.py
-python scripts/3.normalize.py
-python scripts/4.feature_select.py
+# run jupyter notebooks
+jupyter nbconvert --to=html \
+    --FilesWriter.build_directory=scripts/html \
+    --ExecutePreprocessor.kernel_name=python3 \
+    --ExecutePreprocessor.timeout=10000000 \
+    --execute 0.merge_sc_cytotable
+
+jupyter nbconvert --to=html \
+    --FilesWriter.build_directory=scripts/html \
+    --ExecutePreprocessor.kernel_name=python3 \
+    --ExecutePreprocessor.timeout=10000000 \
+    --execute 1.pycytominer_bulk_pipelines.ipynb
+
+jupyter nbconvert --to=html \
+    --FilesWriter.build_directory=scripts/html \
+    --ExecutePreprocessor.kernel_name=python3 \
+    --ExecutePreprocessor.timeout=10000000 \
+    --execute 2.pycytominer_singlecell_pipelines.ipynb
